4e1eee5e91a1898db803e726ef4d8711a2f3cd07
ysy <yisinoya@163.com>
2024-12-11 17:56:40 +0800

    黑马点评-解决缓存穿透、缓存击穿-day4

--- src/main/java/com/hmdp/utils/RedisData.java
+++ src/main/java/com/hmdp/utils/RedisData.java
@@ -4,8 +4,12 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 
+/**
+ * 逻辑过期时间添加
+ */
 @Data
 public class RedisData {
     private LocalDateTime expireTime;
+    // 用来存 Shop
     private Object data;
 }
