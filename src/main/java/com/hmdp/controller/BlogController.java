f944a84d72457c6799c59b70d416239943607888
ysy <yisinoya@163.com>
2025-01-10 20:42:13 +0800

    Revert "黑马点评-达人探店-day12"
    This reverts commit fef4894e9945de4959b6fa5438ec69de7bdc89fd.

--- src/main/java/com/hmdp/controller/BlogController.java
+++ src/main/java/com/hmdp/controller/BlogController.java
@@ -29,7 +29,8 @@ public class BlogController {
 
     @Resource
     private IBlogService blogService;
-
+    @Resource
+    private IUserService userService;
 
     @PostMapping
     public Result saveBlog(@RequestBody Blog blog) {
@@ -44,7 +45,10 @@ public class BlogController {
 
     @PutMapping("/like/{id}")
     public Result likeBlog(@PathVariable("id") Long id) {
-        return blogService.likeBlog(id);
+        // 修改点赞数量
+        blogService.update()
+                .setSql("liked = liked + 1").eq("id", id).update();
+        return Result.ok();
     }
 
     @GetMapping("/of/me")
@@ -61,15 +65,19 @@ public class BlogController {
 
     @GetMapping("/hot")
     public Result queryHotBlog(@RequestParam(value = "current", defaultValue = "1") Integer current) {
-        return blogService.queryHotBlog(current);
-    }
-    @GetMapping("/{id}")
-    public Result queryBlogById(@PathVariable("id") Long id){
-        return blogService.queryBlogById(id);
-    }
-
-    @PutMapping("/like/{id}")
-    public Result queryBlogLikes(@PathVariable("id") Long id) {
-        return blogService.queryBlogLikes(id);
+        // 根据用户查询
+        Page<Blog> page = blogService.query()
+                .orderByDesc("liked")
+                .page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
+        // 获取当前页数据
+        List<Blog> records = page.getRecords();
+        // 查询用户
+        records.forEach(blog ->{
+            Long userId = blog.getUserId();
+            User user = userService.getById(userId);
+            blog.setName(user.getNickName());
+            blog.setIcon(user.getIcon());
+        });
+        return Result.ok(records);
     }
 }
